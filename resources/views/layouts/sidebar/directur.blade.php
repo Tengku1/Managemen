1fb54d0c93570a4308547d827754671b5daf5a02
Tengku1 <anountengku@gmail.com>
2021-10-26 20:12:54 +0800

    Pembelian dibayar dimuka

--- resources/views/layouts/sidebar/directur.blade.php
+++ resources/views/layouts/sidebar/directur.blade.php
@@ -2,7 +2,7 @@
     <a class="nav-link text-white   {{ Request::segment(1) == 'pembelian-dimuka' ? 'bg-gradient-primary active' : '' }}"
         href="{{ route('pembelian-dimuka.get') }}">
         <i class="fa fa-user"></i>
-        <span class="nav-link-text ms-1">Pembayaran Dimuka</span>
+        <span class="nav-link-text ms-1">Pembelian Dimuka</span>
     </a>
 </li>
 <li class="nav-item">
--- resources/views/utility/Modals/PembelianDimuka.blade.php
+++ resources/views/utility/Modals/PembelianDimuka.blade.php
@@ -1,46 +1,23 @@
-<form action="{{ route('faktur.store') }}" method="POST">
+<form action="{{ route('pembelian-dimuka.store') }}" method="POST">
     @csrf
     <div class="form-group row">
         <div class="col-md-6">
-            <label for="title">Nama Barang</label>
-            <input type="text" class="form-control" name="nama_barang" required>
+            <label for="title">No Kasbon</label>
+            <input type="number" class="form-control" name="no_kasbon" required>
         </div>
         <div class="col-md-6">
-            <label for="title">Nama Pemasok</label>
-            <input type="text" class="form-control" name="nama_pemasok" required>
-        </div>
-    </div>
-    <div class="form-group row">
-        <div class="col-md-6">
-            <label for="title">Kode Pemasok</label>
-            <input type="text" class="form-control" name="kode_pemasok" required>
-        </div>
-        <div class="col-md-6">
-            <label for="title">Harga Satuan</label>
-            <input type="number" class="form-control" name="harga_satuan" required>
-        </div>
-    </div>
-    <div class="form-group row">
-        <div class="col-md-4">
-            <label for="title">No Purchase Order</label>
-            <input type="number" class="form-control" name="no_po" required>
-        </div>
-        <div class="col-md-4">
-            <label for="title">Total Harga</label>
-            <input type="number" class="form-control" name="total_harga" required>
-        </div>
-        <div class="col-md-4">
-            <label for="title">Validasi</label>
-            <input type="text" class="form-control" name="validasi" required>
+            <label for="title">No Faktur Pembelian</label>
+            <input type="number" class="form-control" name="no_faktur_pembelian" required>
         </div>
     </div>
     <div class="form-group">
-        <label for="title">Tanggal Faktur</label>
-        <input type="date" class="form-control" name="tgl_faktur" required>
+        <label for="title">Tanggal Kasbon</label>
+        <input type="date" class="form-control" name="tgl_kasbon" required>
     </div>
     <div class="form-group mb-3">
-        <label for="title">Alamat Pemasok</label>
-        <textarea class="form-control" name="alamat_pemasok" required></textarea>
+        <label for="title">Nominal</label>
+        <input type="number" class="form-control" name="nominal" required>
+
     </div>
     @include('utility.btnModal')
 </form>
